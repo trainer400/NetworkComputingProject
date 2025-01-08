7e37e563daf458731b3de1a6fd60e2f5033b176d
trainer400 <matteo.pignataro@tiscali.it>
2025-01-08 13:09:29 +0100

    [Send.py] Added server balance tracking and random packet sending

--- project/send.py
+++ project/send.py
@@ -7,9 +7,15 @@ import random
 from subprocess import Popen, PIPE
 import re
 import argparse
+import yaml
 
 from scapy.all import sendp, get_if_list, get_if_hwaddr
 from scapy.all import Ether, IP, UDP, TCP, Dot1Q
+
+class ServerStats:
+    flows = 0
+    packets = 0
+    to_send_packets = 0
 
 def get_if(interface : str):
     ifs=get_if_list()
@@ -33,38 +39,77 @@ def get_dst_mac(ip):
     except:
         return None
 
-def main():
-    parser = argparse.ArgumentParser(description='Script to send packets to a specific destination')
-    parser.add_argument("-d", "--destination", required=True, type=str, help="The IP address of the destination")
-    parser.add_argument("-p", "--packets", type=int, required=True, help="Number of packets to send")
-    parser.add_argument("-m", "--message", type=str, required=True, help="Message to send")
-    parser.add_argument("-i", "--interface", type=str, required=True, help="Network interface over which the messages have to be sent")
+def score(server: ServerStats) -> int: 
+    return 0 if server.flows == 0 else int(server.packets / server.flows)
+
+def find_best_server(servers: list[ServerStats]):
+    best_index = 0
+    best_score = score(servers[0])
+    for s in range(len(servers)):
+        current_score = score(servers[s])
+
+        if current_score < best_score:
+            best_index = s
+            best_score = current_score
     
-    args = parser.parse_args()
+    return best_index
 
-    ip_addr = args.destination
-    packets = args.packets
-    message = args.message
-    interface = args.interface
-
-    addr = socket.gethostbyname(ip_addr)
-    iface = get_if(interface)
-
+def send_packet(vip: str, src_port: int, message: str):
+    addr = socket.gethostbyname(vip)
+    iface = get_if("veth1")
     tos = 0
-
     ether_dst = get_dst_mac(addr)
 
     if not ether_dst:
         print("Mac address for %s was not found in the ARP table" % addr)
         exit(1)
 
-    for _ in range(packets):
-        # Setup the packet
-        pkt = Ether(src=get_if_hwaddr(iface), dst=ether_dst)
-        pkt = pkt /IP(dst=addr,tos=tos) /UDP(dport=123 + _) /message
-        
-        print("Sending on interface %s to %s port: %s" % (iface, str(addr), 123 +_))
-        sendp(pkt, iface=iface, verbose=False)
+    # Setup the packet
+    pkt = Ether(src=get_if_hwaddr(iface), dst=ether_dst)
+    pkt = pkt /IP(dst=addr,tos=tos) /UDP(sport=src_port, dport=8000) /message
+    sendp(pkt, iface=iface, verbose=False)
+
+def main():
+    parser = argparse.ArgumentParser(description='Script to send packets to a specific destination')
+    parser.add_argument("-y", "--yaml", required=False, type=str,default="config.yaml", help="The yaml configuration file [default: config.yaml]")
+    parser.add_argument("-f", "--flows", type=int, required=True, help="Number of flows to generate")
+    args = parser.parse_args()
+
+    # Get the passed arguments
+    yaml_file = args.yaml
+    flows = args.flows
+
+    # Load the configuration yaml file
+    with open(yaml_file, "r") as file:
+        yaml_content = yaml.safe_load(file)
+    
+    # Load the configuration from the yaml file
+    vip = yaml_content["vip"]
+    backend_number = len(yaml_content["backends"])
+
+    # Track the server_stats stats
+    stats = [ServerStats() for s in range(backend_number)]
+
+    # For each new flow, send an arbitrary number of packets from 5 to 10000
+    for _ in range(flows):
+        # Determinant in discriminating the flows
+        src_port = _ + 8000
+
+        # Number of packet per flow
+        num_packet = random.randint(5, 300)
+
+        # Find the estimated best server
+        best = find_best_server(stats)
+
+        # Update the stats
+        stats[best].flows += 1
+        stats[best].packets += num_packet
+        stats[best].to_send_packets = num_packet
+
+        print(f"Flow {_}, sending {num_packet} packets -> {best}")
+
+        for n in range(num_packet):
+            send_packet(vip, src_port, "Test")
 
 
 if __name__ == '__main__':
